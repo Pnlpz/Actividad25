2b9231147dd05db2c4829259b7ff7689741af30f
Marisol Smith <marisolsmith@gmail.com>
2018-06-13 22:02:05 -0400

    second

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -5,6 +5,15 @@ class SalesController < ApplicationController
 
 def create
   @sale = Sale.new(sale_params)
+  @sale.total = @sale.value - @sale.discount
+  if @sale.tax == 0
+    @sale.total *= 1.19
+    @sale.tax = 19
+  else
+    @sale.tax = 0
+
+  end
+
   @sale.save
   redirect_to sales_new_path
 end
@@ -12,6 +21,7 @@ end
 def show
   @sale = Sale.find(params[:id])
 end
+
   private
   def sale_params
     params.require(:sale).permit( :detail, :category, :value, :discount, :tax, :id)
